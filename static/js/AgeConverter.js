a42caaf282a81000e5e3e5256921b4d0521243b6
nathanforester <nathanforester32@gmail.com>
2021-07-14 14:30:12 +0100

    new game added

--- static/js/AgeConverter.js
+++ static/js/AgeConverter.js
@@ -12,3 +12,8 @@ h1.setAttribute('id', 'Age');
 h1.appendChild(answer);
 document.getElementById('flex-box-result').appendChild(h1);
 }
+
+function reset() {
+    document.getElementById('Age').remove();
+}
+
