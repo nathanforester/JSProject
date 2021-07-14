8b5c1a342a8091fd3dd6c93ca5f0659f1453ba36
nathanforester <nathanforester32@gmail.com>
2021-07-14 17:12:35 +0100

    failed to fully debug as yet

--- static/js/RockScissorPaper.js
+++ static/js/RockScissorPaper.js
@@ -1,10 +1,10 @@
 function rpsGame (yourChoice) {
-    var humanChoice, botChoice;
+    var humanChoice, botChoice; //was bot choice
     humanChoice = yourChoice.id;
-    botChoice = numberSelect(randRPSInt());
-    results = decideWinner(humanChoice, botChoice);
+    botChoice = numberSelect(randRPSInt()); //was bot choice
+    results = decideWinner(humanChoice, botChoice); //was bot choice
     message = finalMessage(results);
-    rpsFontEnd(yourChoice.id, botChoice, message);
+    rpsFontEnd(yourChoice.id, computerChoice, message); //was bot choice
 }
 function randRPSInt () {
     return Math.floor(Math.random() * 3); // eliminate floats  
@@ -14,14 +14,14 @@ function numberSelect(number) {
     return ['rock', 'paper', 'scissors'][number];
 }
 
-function decideWinner (yourChoice, botChoice) {
+function decideWinner (yourChoice, computerChoice) {
     var rpsDatabase = {
         'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
         'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
         'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0}
     }
-    var yourScore = rpsDatabase[yourChoice][botChoice];
-    var computerScore = rpsDatabase[botChoice][yourChoice];
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
 
     return [yourScore, computerScore];
 }
@@ -53,9 +53,9 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
     var botDiv = document.createElement('div');
     var messageDiv = document.createElement('div');
 
-    humanDiv.innerHTML = "<img src'" + imgData[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba:(37, 50, 233, 1)>"
+    humanDiv.innerHTML = "<img src'" + imgData[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba:(37, 50, 233, 1);'>"
     messageDiv.innerHTML = "<h1 style='colour: " + finalMessage['colour'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
-    botDiv.innerHTML = "<img src'" + imgData[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba:(243, 38, 24, 1)>"
+    botDiv.innerHTML = "<img src'" + imgData[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba:(243, 38, 24, 1);'>"
     
     
     document.getElementById('flex-box-rps-div').appendChild(humanDiv);
