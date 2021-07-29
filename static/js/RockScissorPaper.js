003fa7c3da7d80c4e0a90159e665c43509d97b10
nathanforester <nathanforester32@gmail.com>
2021-07-29 14:55:02 +0100

    progressing

--- static/js/RockScissorPaper.js
+++ static/js/RockScissorPaper.js
@@ -53,9 +53,9 @@ function rpsFrontEnd (humanImageChoice, botImageChoice, finalMessage) {
     var botDiv = document.createElement('div');
     var messageDiv = document.createElement('div');
 
-    humanDiv.innerHTML = "<img src'" + imgData[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba:(37, 50, 233, 1);'>"
-    messageDiv.innerHTML = "<h1 style='colour: " + finalMessage['colour'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
-    botDiv.innerHTML = "<img src'" + imgData[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba:(243, 38, 24, 1);'>"
+    humanDiv.innerHTML = "<img src='" + imgData[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba:(37, 50, 233, 1);'>"
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
+    botDiv.innerHTML = "<img src='" + imgData[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba:(243, 38, 24, 1);'>"
     
     
     document.getElementById('flex-box-rps-div').appendChild(humanDiv);
